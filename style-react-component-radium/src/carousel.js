253613ca632d35c9eb4a245f97da3448fe060137
Zhenghao Jia <cherie-xia@hotmail.com>
2019-09-08 12:03:08 +0800

    css

---
+++ style-react-component-radium/src/carousel.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+import Radium from 'radium'
+import React from 'react'
+
+import styles from './carousel-styles.js'
+
+function renderSlides(props) {
+  return React.Children.map(props.children, (slide, i) => {
+    return React.cloneElement(slide, {
+      style: {
+        ...slide.props.style,
+        width: props.width,
+        left: props.width * (i - props.showIndex)
+      }
+    })
+  })
+}
+
+function Carousel(props) {
+  return (
+    <div style={styles.root}>
+      {renderSlides(props)}
+      {props.nav}
+    </div>
+  )
+}
+
+Carousel.propTypes = {
+  nav: PropTypes.node.isRequired,
+  showIndex: PropTypes.number,
+  width: PropTypes.number
+}
+
+export default Radium(Carousel)
